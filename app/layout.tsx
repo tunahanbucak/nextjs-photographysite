5391181a1d557fc8b422112da2fbd3bf9c8c5703
tunahanbucak <tunahanbucak1@gmail.com>
2024-04-13 16:25:54 +0300

    first commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Header from "@/components/Header";
+import Footer from "@/components/Footer";
+import ThemeProvider from "@/components/ThemeProvider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,8 +18,14 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="en" suppressHydrationWarning>
+      <body className={inter.className}>
+        <ThemeProvider attribute="class" defaultTheme="light">
+          <Header />
+          {children}
+          <Footer />
+        </ThemeProvider>
+      </body>
     </html>
   );
 }
---
+++ components/Header.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ThemeToggler from "./ThemeToggler";
+
+export default function Header() {
+  return (
+    <header>
+      <ThemeToggler />
+    </header>
+  );
+}
